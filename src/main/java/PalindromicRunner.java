4b2ace3038d38bd777f3212c0828f85ab309a1ab
Malia Dy <662772@aacps.org>
2020-11-13 01:56:16 +0000

    Attempt 3 finished output

--- src/main/java/PalindromicRunner.java
+++ src/main/java/PalindromicRunner.java
@@ -1,11 +1,8 @@
 public class PalindromicRunner {
     public static void main(String[] args) {
         // be sure you're testing each method as you work then produce your final output using the toString method
-PalindromicNumberFinder test = new PalindromicNumberFinder(72927);
-// System.out.println(test.reverseNum(72927));
-// System.out.println(test.testPalindromicNum(72927));
-// System.out.println(test.searchForPalindromicNum(72927));
-System.out.println(test);
+PalindromicNumberFinder miles = new PalindromicNumberFinder(72927);
+System.out.println(miles);
     }
 }
 
